7747b5d2874e193789849b413bce51c27fe74973
Lucas <123@gmx.de>
2025-10-28 17:20:14 +0100

    .

--- calculations/forms/hypotenuse.go
+++ calculations/forms/hypotenuse.go
@@ -1,8 +1,11 @@
 package forms
 
+import "math"
+
 // Erwartet die Längen der Katheten eines rechtwinkligen Dreiecks.
 // Liefert die Länge der Hypotenuse.
 func Hypotenuse(a, b float64) float64 {
-	// TODO
-	return 0
+	c := a*a + b*b
+	d := math.Sqrt(c)
+	return d
 }
